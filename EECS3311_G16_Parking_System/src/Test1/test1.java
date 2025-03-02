0dc5aa6f1a7a5e9510947859fb65e05214f3c635
DanielFer21 <ferlisidaniel@gmail.com>
2025-03-02 14:37:54 -0500

    Update test1.java

--- EECS3311_G16_Parking_System/src/Test1/test1.java
+++ EECS3311_G16_Parking_System/src/Test1/test1.java
@@ -1,13 +1,12 @@
 package Test1;
 
-public class test1 {
-
-	public static void main(String[] args) {
+public class test1 
+{
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 		System.out.print("Hello");
 		
 		System.out.print("Bye");
-
 	}
-
 }
